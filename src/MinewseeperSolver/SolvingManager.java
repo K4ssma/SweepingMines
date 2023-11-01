7b832ce3159cff12be5f1238c24772039fe32138
Kassma <doerfer.cedric@gmail.com>
2023-11-01 09:18:07 +0100

    simple start functions

--- src/MinewseeperSolver/SolvingManager.java
+++ src/MinewseeperSolver/SolvingManager.java
@@ -1,5 +1,46 @@
 package MinewseeperSolver;
 
-public class SolvingManager {
+import java.awt.*;
+import java.util.Arrays;
 
+public class SolvingManager {
+    private final Dimension DIM = new Dimension(16, 16);
+    private int[] info;
+    private TileGrid grid;
+    private boolean[] needToSolveIds;
+
+    public void start(){
+        //get GridInfo
+        info = new int[DIM.width * DIM.height];
+        grid = new TileGrid(DIM);
+
+        needToSolveIds = new boolean[DIM.width * DIM.height];
+        Arrays.fill(needToSolveIds, false);
+
+        if(DIM.width >= 5 && DIM.height >= 5){
+            leftClickTile(2, 2);
+        }else{
+            leftClickTile(0, 0);
+        }
+        solve();
+    }
+    private void updateGrid(){
+        //get GridStatus
+    }
+
+    public void solve(){
+
+    }
+
+    private boolean simpleSolve(){
+        boolean somethingChanged = false;
+
+    }
+
+    private void leftClickTile(int x, int y){
+        //click Tile(x/y)
+    }
+    private void rightClickTile(int x, int y){
+        //right click Tile(x/y)
+    }
 }
